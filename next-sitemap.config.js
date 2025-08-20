5bd2f38a76de4d3ad8f4751b50fdb2b949da39db
MarielDtt <marieldruetta@gmail.com>
2025-08-20 17:09:33 -0300

    sitemap creado

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://credizza.com.ar',
+  generateRobotsTxt: true, // también genera robots.txt
+  sitemapSize: 5000,
+};
