cd9afe7557b4e4598815251b6faf9def57ca6bba
MarielDtt <marieldruetta@gmail.com>
2025-09-28 17:35:13 -0300

    Agrego reglas para visualizacion de chat

--- src/components/layout/CrispWidget.tsx
+++ src/components/layout/CrispWidget.tsx
@@ -8,7 +8,7 @@ export default function CrispWidget() {
     return (
 
         <>
-            <Script id="crisp-chat" strategy="lazyOnload">
+            <Script id="crisp-chat" strategy="afterInteractive">
                 {` 
                     window.$crisp=[];
                     window.CRISP_WEBSITE_ID="a5d583e4-c94f-4e90-95c0-e5c9d3486fb8";
