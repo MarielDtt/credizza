e1b35442b463a5ab56bf3edddf8ed5413837a31a
MarielDtt <marieldruetta@gmail.com>
2025-09-28 17:55:58 -0300

    modifico title nuevamente, y agrego modificaciones para chat visual

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Credizza",
+    title: "Credizza | Sitio en construcción 🚧",
     description: "Muy pronto lanzamos nuestra nueva web con toda la información sobre créditos simples, ágiles y seguros para jubilados y pensionados.",
     images: ["/og-construccion.webp"],
   },
