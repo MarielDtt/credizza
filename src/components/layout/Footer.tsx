a8f09ae3551ad349102d575a61887bb3e77bc37c
MarielDtt <marieldruetta@gmail.com>
2026-04-27 00:08:25 -0300

    fix(footer): resolver conflicto de merge en snackbar

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -120,14 +120,9 @@ const Footer = () => {
                     variant="filled"
                     sx={{ width: '100%' }}
                 >
-<<<<<<< HEAD
                     Estamos armando nuestro Instagram 🚧
                     <br />
                     ¡Seguinos y acompañanos en el proceso!
-=======
-                    “Estamos armando nuestro Instagram 🚧
-                    ¡Seguinos y acompañanos en el proceso!”
->>>>>>> df75395eaaa5434a18f9c28e2f5adb8f8dc8ce45
                 </Alert>
             </Snackbar>
         </footer>
